c98bcda586bbadc007c58cf6efeccf108715bb2d
cryptolake <dhiadah@gmail.com>
2022-08-03 18:54:13 +0100

    lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Lockboxes.
+
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1 and
+each box may contain keys to the other boxes.
+"""
+
+
+def canUnlockAll(boxes):
+    """Solve lockboxes problem."""
+    unlocked = {0: True}
+    for i, box in enumerate(boxes):
+        for key in box:
+            if key != i:
+                unlocked[key] = True
+    return len(unlocked) == len(boxes)
