880b692b09531125e9c4c04c3bc11c438762489c
cryptolake <dhiadah@gmail.com>
2022-08-11 22:34:04 +0100

    linked lists again

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -23,7 +23,7 @@ listint_t *insert_node(listint_t **head, int number)
 		new_node->n = number;
 		new_node->next = NULL;
 		*head = new_node;
-		return new_node
+		return new_node;
 	}
 
 	while (h->next && h->next->n < number)
