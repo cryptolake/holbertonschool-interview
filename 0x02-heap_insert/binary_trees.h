4c87381b8ad62c4864b813f1cb16036b85dfbb7f
cryptolake <dhiadah@gmail.com>
2022-08-18 19:53:04 +0100

    create binary tree

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,5 +1,9 @@
-#ifndef HEAP
-#def HEAP
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -10,13 +14,13 @@
  */
 typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 } binary_tree_t;
 
-typedef struct binary_tree_s heap_t;
-
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-#endif
+
+#endif /* _BINARY_TREES_H_ */
