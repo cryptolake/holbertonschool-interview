179f6978a5d97d43998e41d48818128ecfab939d
cryptolake <dhiadah@gmail.com>
2022-09-30 15:35:55 +0100

    faster power implementation

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,26 +1,33 @@
 #include "palindrome.h"
 
-
 /**
-* poww - get power
-* @x: number
-* @y: exponant
-* Return: Result
-**/
-unsigned long poww(unsigned long x, unsigned long y)
+ * ipow - get power
+ * @base: number
+ * @exp: exponant
+ * Return: Result
+ **/
+unsigned long ipow(int base, int exp)
 {
-	if (y == 1)
-		return (x);
-	return (x * poww(x, y - 1));
+	unsigned long result = 1;
+
+	for (;;)
+	{
+		if (exp & 1)
+			result *= base;
+		exp >>= 1;
+		if (!exp)
+			break;
+		base *= base;
+	}
+
+	return (result);
 }
 
-
-
 /**
-* is_palindrome - test whether n is a palindrome
-* @n: the number to test
-* Return: 1 if palindrome 0 if not
-**/
+ * is_palindrome - test whether n is a palindrome
+ * @n: the number to test
+ * Return: 1 if palindrome 0 if not
+ **/
 int is_palindrome(unsigned long n)
 {
 	int len = snprintf(NULL, 0, "%lu", n);
@@ -28,7 +35,7 @@ int is_palindrome(unsigned long n)
 
 	if (len == 1)
 		return (1);
-	tlen = poww(10, len - 1);
+	tlen = ipow(10, len - 1);
 
 	if ((unsigned long)(n / tlen) == (unsigned long)(n % 10))
 		return (is_palindrome(n % tlen / 10));
