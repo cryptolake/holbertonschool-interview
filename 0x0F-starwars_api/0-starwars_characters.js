8d7c920c04af64d4ed48d402ad406b05784e22f0
cryptolake <dhiadah@gmail.com>
2022-11-24 17:44:42 +0100

    Star wars

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,10 +5,12 @@ const args = process.argv;
 
 request(`https://swapi-api.hbtn.io/api/films/${args[2]}`, function (error, response, body) {
   const characters = JSON.parse(body).characters;
-  console.log(error);
+  if (error != null)
+    console.log(error);
   for (const url in characters) {
     request(characters[url], function (error, response, body) {
-      console.log(error);
+      if (error != null)
+        console.log(error);
       console.log(JSON.parse(body).name);
     });
   }
