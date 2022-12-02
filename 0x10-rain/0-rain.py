777e695b8412b345fe623307ea949d8504cef7f3
cryptolake <dhiadah@gmail.com>
2022-12-02 20:58:43 +0100

    Rain down on africaaa

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -2,26 +2,43 @@
 """Rain down on africaaaa!."""
 
 
+def cal_inter(walls, w1, w2):
+    """Calculate intermediate walls."""
+    i = w1+1
+    inter = 0
+    while i < w2:
+        inter += walls[i]
+        i += 1
+    return inter
+
+
 def rain(walls):
     """Rain down on africaaaa!."""
     if len(walls) == 0:
         return 0
 
     water = 0
-    for i, w in enumerate(walls[:-1]):
+    i = 0
+    while i < len(walls) - 1:
+        w = walls[i]
         if w > 0:
             w1 = i
             j = i + 1
             while j < len(walls):
-                if walls[j] > 0:
+                if walls[j] != 0:
                     w2 = j
-                    break
+                    if walls[w2] >= walls[w1]:
+                        break
                 j += 1
+            inter = cal_inter(walls, w1, w2)
             if w1 == w2:
                 break
             if walls[w1] >= walls[w2]:
-                water += walls[w2] * (w2 - w1 - 1)
+                water += walls[w2] * (w2 - w1 - 1) - inter
             else:
-                water += walls[w1] * (w2 - w1 - 1)
+                water += walls[w1] * (w2 - w1 - 1) - inter
+            i += w2 - w1
+        else:
+            i += 1
 
     return water
