9d1497c2314bd27db5a0b485fecc943f062ffbd3
cryptolake <dhiadah@gmail.com>
2022-12-09 00:08:40 +0100

    heap sort

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "sort.h"
 
-void swap(int* a, int* b)
+/**
+* swap - swap two elements
+* @a: element
+* @b: element
+**/
+void swap(int *a, int *b)
 {
 	int temp = *a;
 
@@ -9,6 +14,13 @@ void swap(int* a, int* b)
 	*b = temp;
 }
 
+/**
+* heapify - heapify array
+* @arr: array
+* @N: variable size of array
+* @i: index
+* @size: unchanging size of array
+**/
 void heapify(int *arr, int N, int i, size_t size)
 {
 	int largest = i;
@@ -21,13 +33,19 @@ void heapify(int *arr, int N, int i, size_t size)
 	if (right < N && arr[right] > arr[largest])
 		largest = right;
 
-	if (largest != i) {
+	if (largest != i)
+	{
 		swap(&arr[i], &arr[largest]);
 		print_array(arr, size);
 		heapify(arr, N, largest, size);
 	}
 }
 
+/**
+* heap_sort - perform heap sort
+* @array: array to sort
+* @size: size of array
+**/
 void heap_sort(int *array, size_t size)
 {
 	int i;
@@ -37,7 +55,8 @@ void heap_sort(int *array, size_t size)
 	for (i = size / 2 - 1; i >= 0; i--)
 		heapify(array, size, i, size);
 
-	for (i = size - 1; i > 0; i--) {
+	for (i = size - 1; i > 0; i--)
+	{
 		swap(&array[0], &array[i]);
 		print_array(array, size);
 		heapify(array, i, 0, size);
