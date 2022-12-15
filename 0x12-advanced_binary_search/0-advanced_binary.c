2b6fc9bbb76f9ece5c2ddec94effecb95fca54da
cryptolake <dhiadah@gmail.com>
2022-12-15 23:22:26 +0100

    advanced binary search

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,53 @@
+#include "search_algos.h"
+
+/**
+ * p_search_array - print searched array
+ * @array: array to print part of
+ * @l: first index
+ * @r: last index
+ **/
+void p_search_array(int *array, size_t l, size_t r)
+{
+	printf("Searching in array: ");
+	for (; l < r; l++)
+		printf("%d, ", array[l]);
+	printf("%d\n", array[l]);
+}
+
+
+/**
+ * binary_search_rec - search array using binary search
+ * @array: array to search
+ * @l: beginning
+ * @r: end
+ * @value: value to search for
+ * Return: first index of value
+ **/
+int binary_search_rec(int *array, size_t l, size_t r, int value)
+{
+	size_t mid = l + (r - l) / 2;
+
+	if (l == r)
+	{
+		if (array[l] == value)
+			return (l);
+		return (-1);
+	}
+	p_search_array(array, l, r);
+	if (array[mid] >= value)
+		return (binary_search_rec(array, l, mid, value));
+	else
+		return (binary_search_rec(array, mid + 1, r, value));
+}
+
+/**
+ * advanced_binary - search array using binary search
+ * @array: array to search
+ * @size: size of array
+ * @value: value to search for
+ * Return: first index of value
+ **/
+int advanced_binary(int *array, size_t size, int value)
+{
+	return (binary_search_rec(array, 0, size - 1, value));
+}
---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef SEARCH
+#define SEARCH
+#include <stdio.h>
+#include <stdlib.h>
+int advanced_binary(int *array, size_t size, int value);
+
+#endif
