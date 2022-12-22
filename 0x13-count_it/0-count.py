19ecb1bd232c90e837cddb95c581bcbc2421c8fb
cryptolake <dhiadah@gmail.com>
2022-12-22 02:44:06 +0100

    count it reddit UPGRADED

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -3,29 +3,35 @@
 import requests
 
 
-def count_words(subreddit, word_list, after='', hot_list={}):
+def count_words(subreddit, word_list, after='', hot_list=None):
     """Get posts of sub."""
+    if hot_list is None:
+        hot_list = {item: 0 for item in word_list}
+
+    if after is None:
+        for word in sorted(hot_list, key=lambda x: hot_list[x])[::-1]:
+            if hot_list[word] != 0:
+                print("{}: {}".format(word, hot_list[word]))
+        return None
+
     headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)"}
     url = 'https://www.reddit.com'
     res = requests.get('{}/r/{}/hot.json?after={}?limit=100'
                        .format(url, subreddit, after), headers=headers,
                        allow_redirects=False)
-    if res.status_code == 302:
-        return None
-    rj = res.json()
-    if rj.get('data') is None:
-        return None
-    for post in rj['data']['children']:
-        title = post['data']['title'].lower().split(' ')
-        for word in title:
-            if word in word_list:
-                if hot_list.get(word):
-                    hot_list[word] += 1
-                else:
-                    hot_list[word] = 1
 
-    if rj['data']['after'] is None:
-        for word in sorted(hot_list, key=lambda x: hot_list[x]):
-            print("{}: {}".format(word, hot_list[word]))
+    if res.status_code == 200:
+        rj = res.json()
+        if rj.get('data') is None:
+            return None
+        for post in rj['data']['children']:
+            title = post['data']['title'].lower().split(' ')
+            for word in title:
+                if word in word_list:
+                    if hot_list.get(word):
+                        hot_list[word] += 1
+                    else:
+                        hot_list[word] = 1
 
-    return count_words(subreddit, rj['data']['after'], hot_list)
+        return count_words(subreddit, word_list, rj['data']['after'], hot_list)
+    return None
