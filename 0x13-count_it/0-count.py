799b6e7bcdd861160b0b8f05de1e55d9cbec46a2
cryptolake <dhiadah@gmail.com>
2022-12-26 00:55:14 +0100

    My own fault...

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -5,11 +5,12 @@ import requests
 
 def count_words(subreddit, word_list, after='', hot_list=None):
     """Get posts of sub."""
+    word_list = [item.lower() for item in word_list]
     if hot_list is None:
-        hot_list = {item.lower(): 0 for item in word_list}
+        hot_list = {item: 0 for item in word_list}
 
     if after is None:
-        for word in sorted(hot_list, key=lambda x: hot_list[x])[::-1]:
+        for word in sorted(hot_list, key=lambda x: (hot_list[x], x))[::-1]:
             if hot_list[word] != 0:
                 print("{}: {}".format(word, hot_list[word]))
         return None
@@ -26,8 +27,8 @@ def count_words(subreddit, word_list, after='', hot_list=None):
             return None
         for post in rj['data']['children']:
             title = post['data']['title'].lower().split(' ')
-            for word in title:
-                if word in word_list:
+            for word in word_list:
+                if word in title:
                     hot_list[word] += 1
 
         return count_words(subreddit, word_list, rj['data']['after'], hot_list)
