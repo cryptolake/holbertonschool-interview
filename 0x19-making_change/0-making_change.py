604831d60dd09da392a4cfc48db613a316b2da5a
cryptolake <dhiadah@gmail.com>
2023-02-23 21:44:21 +0100

    WE OUT HERE GETTTIN SOLUTIONNNNNNNNNNNNNNNNNS

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""
-Given a pile of coins of different values,
-determine the fewest number of coins needed to meet a given amount total.
-"""
+"""Determine the fewest number of coins needed to meet a given amount total."""
+
 
 def makeChange(coins, total):
     """Determine the fewest number of coins."""
     i = 0
     for coin in sorted(coins, reverse=True):
         j = total // coin
-        total -= j * coin 
+        total -= j * coin
         i += j
         if total <= 0:
             break
