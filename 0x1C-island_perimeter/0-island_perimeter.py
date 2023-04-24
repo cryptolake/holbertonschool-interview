a98e057107b5499342914e65e02e23c651960324
cryptolake <dhiadah@gmail.com>
2023-04-24 23:50:30 +0100

    update

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -4,9 +4,9 @@
 
 def is_valid(x, y, grid):
     """Test if x, y coordinate are valid."""
-    size = len(grid) - 1
+    line, col = len(grid), len(grid[0])
 
-    if x > size or y > size or x < 0 or y < 0:
+    if x > line or y > col or x < 0 or y < 0:
         return False
     return True
 
@@ -15,7 +15,7 @@ def island_perimeter(grid):
     """Find island Perimeter."""
     per = 0
     for i in range(len(grid)):
-        for j in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 per += 4
                 if is_valid(i+1, j, grid) and grid[i+1][j] == 1:
